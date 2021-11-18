7e2d0932a3987b8bb8e99ad7ac05632ba5ba7a40
AlbertoPimentel <albertopimentel04@gmail.com>
2021-11-17 21:11:18 -0600

    Version 0.0.1 lista

---
+++ test/data/products.ts
@@ -0,0 +1,9 @@
+export const product = {
+    id:"1",
+    title:"Coffe Mug - Sin imagen",
+}
+export const product2 = {
+    id:"1",
+    title:"Coffe Mug - Con Imagen",
+    img:"./coffe-mug.png"
+}
